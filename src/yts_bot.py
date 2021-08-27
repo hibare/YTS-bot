852c996d1b4f2c647a19049d6daba030065308c5
Shubham Hibare <github@hibare.in>
2021-08-27 20:29:03 +0530

    Bug fix + refactor

--- src/yts_bot.py
+++ src/yts_bot.py
@@ -4,6 +4,9 @@ import sys
 import json
 import logging
 from pathlib import Path
+from urllib.parse import urljoin
+from datetime import datetime, timezone
+from typing import Dict
 import requests
 from bs4 import BeautifulSoup
 from decouple import config
@@ -16,8 +19,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def send_notification(text):
-    logger.info("Sending slack notification, data=%s", text)
+def send_notification(text: str) -> int:
+    """Send slack notifications"""
+
+    logger.info("Sending slack notification")
     slack_endpoint = config('SLACK_ENDPOINT')
     headers = {
         'Content-type': 'application/json',
@@ -30,100 +35,105 @@ def send_notification(text):
     return response.status_code
 
 
-def yts():
-    logger.info("Starting run")
-    urls = ["https://yst.am/", "https://yts.vc/", "https://yts.lt/"]
+def extract(url: str) -> Dict:
+    """Extract top movies from link"""
+
+    movies = dict()
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
     }
-    data_dir_path = "/data"
-    movies_history = Path(data_dir_path) / "yts_movie_list.txt"
 
-    Path(data_dir_path).mkdir(parents=True, exist_ok=True)
-
-    for url in urls:
+    try:
         response = requests.get(url, headers=headers)
 
         logger.info("Received respose=%s for URL=%s",
                     response.status_code, url)
 
-        # retrieve content from website
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            popular_downloads_div = soup.find(id='popular-downloads')
-            second_row = popular_downloads_div.find_all('div', class_="row")[1]
-            all_top_movies = second_row.find_all(
+            popular_downloads = soup.find(id='popular-downloads')
+            second_row = popular_downloads.find_all('div', class_="row")[1]
+            top_movies = second_row.find_all(
                 'div', class_="browse-movie-wrap")
 
-            extracted_movies_dict = dict()
-
-            for each_movie in all_top_movies:
-                title = each_movie.find(
+            for movie in top_movies:
+                title = movie.find(
                     'a', class_="browse-movie-title").get_text()
-                movie_link = "%s%s" % (
-                    url.strip("/"), each_movie.find('a', class_="browse-movie-title").get('href'))
-                img_link = "%s%s" % (
-                    url.strip("/"), each_movie.find('img').get('src'))
-
-                extracted_movies_dict.update(
-                    {
-                        title: dict(
-                            movie_link=movie_link,
-                            img_link=img_link,
-                        )
-                    }
+                link = urljoin(
+                    url,
+                    movie.find('a', class_="browse-movie-title").get('href')
+                )
+                cover_image = urljoin(
+                    url, movie.find('img').get('src')
                 )
 
-            logger.info("Extracted movies=%s", extracted_movies_dict.keys())
-
-            # retrieve existing movies
-            try:
-                with open(movies_history) as f:
-                    existing_movie_list = [x.strip() for x in f.readlines()]
-            except FileNotFoundError:
-                existing_movie_list = list()
-
-            extracted_movies_title_list = extracted_movies_dict.keys()
-            new_movies_list = set(
-                extracted_movies_title_list) - set(existing_movie_list)
-            skipped_movies = set(extracted_movies_title_list) - new_movies_list
-            logger.info("Skipped movies: %s", skipped_movies)
-
-            # send notification if new movies available
-            notification_response_dict = dict()
-            if new_movies_list:
-                for new_movie in new_movies_list:
-                    text = """Hey found new movie on YTS\n\n*%s* `<%s|view>`\n\n%s""" % (
-                        new_movie,
-                        extracted_movies_dict.get(
-                            new_movie, {}).get("movie_link"),
-                        extracted_movies_dict.get(
-                            new_movie, {}).get("img_link")
-                    )
-
-                    response_code = send_notification(text)
-                    logger.info(
-                        "Received notification respose=%s for movie=%s", response_code, new_movie)
-
-                    if str(response_code).startswith(('2')):
-                        notification_response_dict.update({
-                            new_movie: response_code
-                        })
-
-                # update the tracker file
-                logger.info("Updating tracker file with movies=%s",
-                            notification_response_dict.keys())
-                if notification_response_dict:
-                    with open(movies_history, 'a') as f:
-                        movie_names = notification_response_dict.keys()
-                        f.write("\n".join(movie_names))
-                        f.write("\n")
-
-            else:
-                logger.info("No new movies found")
+                movies[title] = {
+                    "title": title,
+                    "link": link,
+                    "cover_image": cover_image,
+                    "timestamp": datetime.now(timezone.utc).isoformat()
+                }
 
         else:
-            logger.info("Got response %s" % (response.status_code))
+            logger.error("url=%s, status=%s", url, response.status_code)
+    except Exception:
+        logger.exception("url=%s", url)
+
+    logger.info("url=%s, movies=%s", url, movies.keys())
+    return movies
+
+
+def yts(data_dir: str = "/data", history: str = "yts_movie_list.txt") -> None:
+    urls = ["https://yst.am/", "https://wvw.yts.vc/yify/", "https://yts.lt/"]
+
+    logger.info("Starting run")
+
+    Path(data_dir).mkdir(parents=True, exist_ok=True)
+    history_path = Path(data_dir) / history
+
+    movies = dict()
+    history = dict()
+
+    for url in urls:
+        movies.update(extract(url))
+
+    # Fetch history
+    try:
+        with open(history_path) as f:
+            history = json.loads(f.read())
+    except Exception:
+        pass
+
+    # Check for new movies
+    new_movies = set(movies.keys()) - set(history.keys())
+
+    logger.info("New movies %s", new_movies)
+
+    if new_movies:
+        # Send notification & update history file
+        for title in new_movies:
+            details = movies.get(title)
+            text = """Hey found new movie on YTS\n\n*%s* `<%s|view>`\n\n%s""" % (
+                title,
+                details.get("link"),
+                details.get("cover_image")
+            )
+
+            try:
+                response_code = send_notification(text)
+                logger.info("Received notification respose=%s for movie=%s",
+                            response_code, title)
+
+                if str(response_code).startswith(('2')):
+                    history[title] = details
+            except Exception:
+                logger.exception(
+                    "Failed to send notification, movie=%s", title)
+
+        # Write new history
+        with open(history_path, 'w') as f:
+            f.write(json.dumps(history))
 
 
 def main():
@@ -140,8 +150,8 @@ def main():
     logger.info("Adding job")
     logger.info("Interval=%s", interval)
 
-    shed.add_job(yts, 'interval', hours=interval)
-    # shed.add_job(yts, 'interval', minutes=1)
+    # shed.add_job(yts, 'interval', hours=interval)
+    shed.add_job(yts, 'interval', minutes=1)
 
     try:
         logger.info("Starting scheduler")
